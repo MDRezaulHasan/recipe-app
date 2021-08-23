3a8df245215d3f9b57f001ef5390d278eaeaf2bc
Ajoy234 <hampr.ajoy@gmail.com>
2021-08-24 01:22:47 +0600

    first uploded

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import React, { useEffect, useState} from "react";
+import Recipe from "./Recipe";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+
+  const APP_ID = "90796b1c";
+  const APP_KEY = "f4dbcf670eb6178fc83243fa3d5037b1";
+  
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('chicken');
+  useEffect(() => {
+    getRecipes();
+  },[query]);
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const data = await response.json();
+    // console.log(data);
+    setRecipes(data.hits);
+    console.log(" Response Data"+data.hits);
+
+  };
+
+  const updateSearch = e =>{
+    setSearch(e.target.value);
+    //console.log(search);
+  }
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search); 
+    setSearch('');
+    }
+
+
+  return(
+    <div className ="App">
+      <form onSubmit ={getSearch} className = "search-form">
+        <input className = "search-bar" type="text" value={search} onChange={updateSearch}/>
+        <button className  = "search-button" type="submit">Search</button>
+      </form>
+      <div className="recipes"> 
+      {recipes.map(recipe => (
+        <Recipe 
+        key={recipe.recipe.label}
+        title={recipe.recipe.label}
+        calories={recipe.recipe.calories}
+        image={recipe.recipe.image}
+        ingredients={recipe.recipe.ingredients}
+        />
+      ))}
+      </div>
     </div>
   );
-}
+};
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//      <h1>Recipe</h1>
+//     </div>
+//   );
+// }
 
 export default App;
